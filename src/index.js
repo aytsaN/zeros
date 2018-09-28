c71dce16877bc2a4693626bffffaf16a45673789
aytsaN <aytsan.shamal@gmail.com>
2018-09-28 21:54:11 +0300

    feature function getZerosCount

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 module.exports = function getZerosCount(number) {
 
-    var countZeros = 0;
-    var num = Math.floor(number / 5);
-    for(;num != 0;){
+    let countZeros = 0;
+    let num = Math.floor(number / 5);
+
+    while(num){
         countZeros += num;
         num = Math.floor(num / 5);
     }
